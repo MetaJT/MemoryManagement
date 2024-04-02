00002ea7b428127d1e614afe1d117721d996c23a
metaJT <jordantrotter3@gmail.com>
2024-04-02 14:29:38 -0500

    Initial Commit

---
+++ MemoryManagement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MemoryManagement
+//
+//  Created by Jordan Trotter on 4/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
